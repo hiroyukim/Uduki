2e5814d9cfcc37fcff68754414311e33e243cfce
yamanaka <hiroyukimm@gmail.com>
2012-07-17 10:59:57 +0900

    top pageでの日記生成処理を追加

--- lib/Uduki/Web/Dispatcher.pm
+++ lib/Uduki/Web/Dispatcher.pm
@@ -3,16 +3,33 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::Lite;
+use Uduki::DateTime;
 
 any '/' => sub {
     my ($c) = @_;
-    $c->render('index.tt');
-};
 
-post '/account/logout' => sub {
-    my ($c) = @_;
-    $c->session->expire();
-    $c->redirect('/');
+    my $today = Uduki::DateTime->today();
+
+    my $diary = $c->dbh->query('SELECT id,created_on FROM diary WHERE created_on = ?',$today->strftime("%Y-%m-%d"))->hash;
+
+    unless($diary) {
+        $c->dbh->begin_work();
+        try {
+            $c->dbh->do('INSERT INTO diary (body,created_on) VALUES('',?)',{}, 
+                $today->strftime("%Y-%m-%d")
+            );
+            $c->dbh->commit();
+        }
+        catch {
+            my $err = shift;
+            $c->dbh->rollback();
+            Carp::croak($err);
+        };
+    }
+    
+    $c->redirect('/diary/edit',{
+        created_on => $today->strftime("%Y-%m-%d"),
+    });
 };
 
 1;
--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,18 @@
+DROP   DATABASE if exists uduki;
+CREATE DATABASE uduki DEFAULT CHARACTER SET utf8;
+
+use uduki;
+
 CREATE TABLE IF NOT EXISTS sessions (
     id           CHAR(72) PRIMARY KEY,
     session_data TEXT
 );
+
+CREATE TABLE diary (
+  id           int(10) unsigned NOT NULL auto_increment,
+  body         TEXT,
+  created_on   date         NOT NULL,
+  updated_at   TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX created_on (created_on)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
