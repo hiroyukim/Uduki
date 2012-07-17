e46cd83529433590d4878971514f6141ec17aaaa
yamanaka <hiroyukimm@gmail.com>
2012-07-17 10:14:21 +0900

    init module

---
+++ lib/Uduki.pm
@@ -0,0 +1,11 @@
+package Uduki;
+use strict;
+use warnings;
+use utf8;
+use parent qw/Amon2/;
+our $VERSION='0.01';
+use 5.008001;
+
+__PACKAGE__->load_plugin(qw/DBI/);
+
+1;
---
+++ t/00_compile.t
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+
+use_ok $_ for qw(
+    Uduki
+    Uduki::Web
+    Uduki::Web::Dispatcher
+);
+
+done_testing;
