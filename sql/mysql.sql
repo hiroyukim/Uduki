0dbefa37e4714fd425c9ff2c29ac0716140ea11c
yamanaka <hiroyukimm@gmail.com>
2012-08-22 17:01:59 +0900

    comment out DROP CREATE use

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,7 +1,6 @@
-DROP   DATABASE if exists uduki;
-CREATE DATABASE uduki DEFAULT CHARACTER SET utf8;
-
-use uduki;
+-- DROP   DATABASE if exists uduki;
+-- CREATE DATABASE uduki DEFAULT CHARACTER SET utf8;
+-- use uduki;
 
 CREATE TABLE IF NOT EXISTS sessions (
     id           CHAR(72) PRIMARY KEY,
