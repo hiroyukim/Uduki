8bccc3471e838bced920c6edc96317cb4988258f
yamanaka <hiroyukimm@gmail.com>
2012-08-30 20:39:48 +0900

    tag機能を消して検索周りをJS化させた

--- lib/Uduki/Web/Dispatcher.pm
+++ lib/Uduki/Web/Dispatcher.pm
@@ -116,11 +116,6 @@ get '/api/diary/search' => sub {
         }],
     ));
 
-    if( my $tag_id = $c->req->param('tag_id') ) {
-        my @diary_tags = $c->dbh->query('SELECT diary_id FROM diary_tag WHERE tag_id = ?',$tag_id)->hashes; 
-        $cond->{id} = { -in => [map { $_->{diary_id} } @diary_tags ] };
-    }
-
     if( my $word = $c->req->param('word') ) {
         if( $c->config->{full_text_search} ) { 
             $cond->{body} = { -match => $word }
@@ -150,117 +145,6 @@ get '/api/diary/search' => sub {
         data   => $diaries, 
         pager  => { map { $_ => $pager->$_ } qw/total_entries entries_per_page current_page entries_on_this_page first_page last_page first last previous_page next_page/ },
     });
-};
-
-=pod
-get '/api/tag/list' => sub {
-    my ($c) = @_;
-
-    my @diary_tags = $c->dbh->query('SELECT tag_id FROM diary_tag WHERE diary_id = ?',$c->req->param('diary_id') )->hashes; 
-
-    return $c->render_json(+{
-        data => @diary_tags 
-            ? [$c->dbh->query($c->dbh->abstract->select('tag',[qw/id name/],{ id => { -in => [map { $_->{tag_id} } @diary_tags ] } }))->hashes] 
-            : []
-    });         
-};
-=cut
-
-post '/api/tag/edit' => sub {
-    my ($c) = @_;
-
-    my $tag_name   = $c->req->param('tag_name');
-    my $created_on = $c->req->param('created_on');
-    
-    unless( $c->dbh->query('SELECT * FROM diary WHERE created_on = ?',$created_on )->hash ) {
-        $c->dbh->do(q{INSERT INTO diary (body,created_on) VALUES('',?)},{}, 
-            $created_on
-        );
-    }
-
-    my $tag   = $c->dbh->query('SELECT * FROM tag WHERE name = ?',$tag_name)->hash; 
-    my $diary = $c->dbh->query('SELECT id FROM diary WHERE created_on = ?',$created_on)->hash; 
-
-    unless( $tag ) {
-        $c->dbh->begin_work();
-        try {
-            $c->dbh->do(q{INSERT INTO tag (name) VALUES(?)},{}, 
-                $tag_name,
-            );
-            $c->dbh->commit();
-        }
-        catch {
-            my $err = shift;
-            $c->dbh->rollback();
-            Carp::croak($err);
-        };
-       
-        $tag = $c->dbh->query('SELECT * FROM tag WHERE name = ?',$tag_name)->hash;
-    };
-
-    if( $c->dbh->query('SELECT * FROM diary_tag WHERE diary_id = ? AND tag_id = ?',$diary->{id},$tag->{id})->hash ) { 
-        $c->dbh->begin_work();
-        try {
-            $c->dbh->do(q{DELETE FROM diary_tag WHERE diary_id = ? AND tag_id = ?},{}, 
-                $diary->{id},
-                $tag->{id},
-            );
-            
-            my $diary_tag = $c->dbh->query('SELECT COUNT(id) AS count FROM diary_tag WHERE diary_id != ? AND tag_id = ?',
-                $diary->{id},
-                $tag->{id},
-            )->hash;
-            
-            if( int($diary_tag->{count}) == 0 ) {
-                $c->dbh->do(q{DELETE FROM tag WHERE id = ?},{}, 
-                    $tag->{id},
-                );
-            }
-
-            $c->dbh->commit();
-        }
-        catch {
-            my $err = shift;
-            $c->dbh->rollback();
-            Carp::croak($err);
-        };
-    }
-    else {
-        $c->dbh->begin_work();
-        try {
-            $c->dbh->do(q{INSERT INTO diary_tag (diary_id,tag_id) VALUES(?,?)},{}, 
-                $diary->{id},
-                $tag->{id},
-            );
-            $c->dbh->commit();
-        }
-        catch {
-            my $err = shift;
-            $c->dbh->rollback();
-            Carp::croak($err);
-        };
-    }
-
-    $c->render_json(+{
-        result => 'ok',
-    });
-};
-
-get '/api/tag/list' => sub {
-    my ($c) = @_;
-
-    my $diary = $c->dbh->query('SELECT id,created_on FROM diary WHERE created_on = ?',$c->req->param('created_on'))->hash;
-
-    if( my @diary_tags = $c->dbh->query('SELECT tag_id FROM diary_tag WHERE diary_id = ?',$diary->{id})->hashes ) {
-        $c->render_json(+{
-            tags => [$c->dbh->query($c->dbh->abstract->select('tag',[qw/id name/],{ id => { -in => [map { $_->{tag_id} } @diary_tags ] } }))->hashes], 
-        });
-    }
-    else {
-        $c->render_json(+{
-            tags => [], 
-        });
-    }
 };
 
 1;
